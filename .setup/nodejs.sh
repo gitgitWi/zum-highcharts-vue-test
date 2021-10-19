cac83b6826c44e918f48e6512ae95cb0b7eb791d
gitgitWi <wiii@kakao.com>
2021-10-19 11:05:08 +0900

    Build: modify some setup configs

--- .setup/nodejs.sh
+++ .setup/nodejs.sh
@@ -11,5 +11,6 @@ npm i -g npm
 npm i -g yarn
 
 yarn global add pretty-quick lint-staged eslint prettier husky @vue/cli
+npx husky install
 npx husky set .husky/pre-commit "npx pretty-quick --staged"
 npx husky add .husky/pre-commit "npx lint-staged"
