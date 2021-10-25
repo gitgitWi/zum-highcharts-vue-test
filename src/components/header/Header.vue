a1998f1549940cc0f6179ad508be22f5701ccb1b
gitgitWi <wiii@kakao.com>
2021-10-25 16:18:07 +0900

    Feat/Header: add last-build datetime using env

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
     <a href="https://zum-test.web.app"><h1>ZUM 국내/해외증시 MAP 테스트</h1></a>
+    <span id="last-build">
+      last build at <b>{{ lastBuild }}</b>
+    </span>
   </header>
 </template>
 
@@ -8,25 +11,40 @@
 import Vue from "vue";
 export default Vue.extend({
   name: "Header",
+
+  data() {
+    return {
+      lastBuild: process.env.VUE_APP_LAST_BUILD,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 header {
-  a {
-    text-decoration: none;
-    color: rgb(27, 82, 237);
+  margin-bottom: 15px;
+}
 
-    h1 {
-      font-weight: 500;
-    }
+a {
+  text-decoration: none;
+  color: rgb(27, 82, 237);
 
-    :hover {
-      font-weight: 900;
-
-      transition-property: font-weight;
-      transition-duration: 200ms;
-    }
+  h1 {
+    font-weight: 500;
   }
+
+  :hover {
+    font-weight: 900;
+    transition-property: font-weight;
+    transition-duration: 100ms;
+  }
+}
+
+#last-build {
+  height: max-content;
+  margin: 5px;
+
+  font-size: 16px;
+  font-style: italic;
 }
 </style>
