6399bbfe5af54a61309db394dad2da0be9d121e3
gitgitWi <wiii@kakao.com>
2021-10-29 03:55:46 +0900

    Feat/Fetcher: get data about sectors

--- server/index.ts
+++ server/index.ts
@@ -2,34 +2,30 @@ import { writeFileSync } from "fs";
 import { ZumFetcher } from "./services";
 import { wantedKospi, wantedKosdaq } from "./constants";
 
+import krSectors from "./assets/kr-sectors.json";
+
 async function main() {
   console.log(`__Fetcher Started__`);
 
-  // console.log(
-  //   await ZumFetcher.searchStockCodes(
-  //     wantedKosdaq.map(({ stockName }) => stockName)
-  //   )
+  // const kospi = ZumFetcher.getStockData(
+  //   wantedKospi.map(({ stockCode }) => stockCode)
+  // );
+  // const kosdaq = ZumFetcher.getStockData(
+  //   wantedKosdaq.map(({ stockCode }) => stockCode)
   // );
 
-  const kospi = ZumFetcher.getStockData(
-    wantedKospi.map(({ stockCode }) => stockCode)
-  );
-  const kosdaq = ZumFetcher.getStockData(
-    wantedKosdaq.map(({ stockCode }) => stockCode)
-  );
+  // Promise.all([kospi, kosdaq]).then(([kospiResult, kosdaqResult]) => {
+  //   const scraped = JSON.stringify({
+  //     stocks: [
+  //       ...kospiResult.map((data) => ({ category: "KOSPI", ...data })),
+  //       ...kosdaqResult.map((data) => ({ category: "KOSDAQ", ...data })),
+  //     ],
+  //   });
 
-  Promise.all([kospi, kosdaq]).then(([kospiResult, kosdaqResult]) => {
-    const scraped = JSON.stringify({
-      stocks: [
-        ...kospiResult.map((data) => ({ category: "KOSPI", ...data })),
-        ...kosdaqResult.map((data) => ({ category: "KOSDAQ", ...data })),
-      ],
-    });
-
-    writeFileSync(`../assets/kr-dummy.json`, scraped, {
-      flag: "w",
-    });
-  });
+  //   writeFileSync(`../assets/kr-dummy.json`, scraped, {
+  //     flag: "w",
+  //   });
+  // });
 }
 
 main();
