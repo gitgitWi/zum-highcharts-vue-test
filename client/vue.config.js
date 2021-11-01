fdb973e33cba9a20b3ccb752a765786940730159
gitgitWi <wiii@kakao.com>
2021-11-01 11:50:33 +0900

    Fix/Build: add vue cli path alias

--- client/vue.config.js
+++ client/vue.config.js
@@ -8,6 +8,7 @@ const configs = {
   configureWebpack: {
     resolve: {
       alias: {
+        "@/": path.join(__dirname, "src"),
         $assets: path.join(__dirname, "src", "assets"),
       },
     },
